035a8b56f7a3ef0a6954ea769168791ad8878d5d
Blaink <v4lenj4u@gmail.com>
2025-12-05 23:47:26 -0300

    Initialize vehicle data structure with autos
    Added a collection of vehicles with details including name, description, and image for each type.

---
+++ js/autos.js
@@ -0,0 +1,334 @@
+const vehicles = {
+    autos: [
+        {
+            name: "bentayga4x4",
+            description: "Full Blindado",
+            image: "https://i.postimg.cc/FH5NqgxL/bentayga4x4.png"
+        },
+        {
+            name: "x6mf96lbwk",
+            description: "Blindaje de ruedas",
+            image: "https://i.postimg.cc/2jQsyRdn/image.png"
+        },
+        {
+            name: "fortuner",
+            description: "Full Blindado + 50 Tn de Peso",
+            image: "https://i.postimg.cc/TPvjpSJt/image.png"
+        },
+        {
+            name: "gcars_r32_tuned_kr3sk1",
+            description: "Blindaje de ruedas",
+            image: "https://i.postimg.cc/tTZVkN5f/imagen-2025-12-05-203609447.png",
+        },
+        {
+            name: "surfpepsiv4",
+            description: "Blindaje de ruedas + suspension hidraulica manejable y posee texturas bugueadas, que se arreglan en el mecanico",
+            image: "https://i.postimg.cc/C1z64X98/image.png",
+        },
+        {
+            name: "24mdxtech",
+            description: "Blindaje de ruedas",
+            image: "https://i.postimg.cc/rFSZgSZR/image.png",
+        },
+        {
+            name: "23x5",
+            description: "Full Blindaje + 50 Tn de Peso",
+            image: "https://i.postimg.cc/J4YNm0Fw/image.png",
+        },
+        {
+            name: "rmodx6",
+            description: "Blindaje de ruedas",
+            image: "https://i.postimg.cc/CxWbBtSF/image.png",
+        },
+        {
+            name: "m8xmoon",
+            description: "Addon Normal",
+            image: "https://i.postimg.cc/1X5F11cq/image.png",
+        },
+        {
+            name: "infss",
+            description: "Full Blindado",
+            image: "https://i.postimg.cc/76XCVQ8P/image.png",
+        },
+        {
+            name: "supraryf",
+            description: "Addon Normal",
+            image: "https://i.postimg.cc/y8WwYJ8K/image.png",
+        },
+        {
+            name: "nisgtir",
+            description: "Full Blindado",
+            image: "https://i.postimg.cc/dtVf47dt/image.png",
+        },
+        {
+            name: "ER34N",
+            description: "Blindaje de ruedas + full detalle con infinidad de calcomanias",
+            image: "https://i.postimg.cc/L51Jp4g9/image.png",
+        },
+        {
+            name: "peugeot3008",
+            description: "Blindaje de ruedas",
+            image: "https://i.postimg.cc/SjbJN0Wp/image.png",
+        },
+        {
+            name: "isetta",
+            description: "Addon Normal",
+            image: "https://i.postimg.cc/Cx1RNVwc/image.png",
+        },
+        {
+            name: "yRenault5TB",
+            description: "Blindaje de ruedas",
+            image: "https://i.postimg.cc/g2TntB0K/image.png",
+        },
+        {
+            name: "tbird",
+            description: "Blindaje de ruedas",
+            image: "https://i.postimg.cc/P5RPVFq4/image.png",
+        },
+        {
+            name: "hycf40",
+            description: "Full Blindado",
+            image: "https://i.postimg.cc/L8bnQ0s3/image.png",
+        },
+        {
+            name: "crossfoxjuani",
+            description: "Blindaje de ruedas",
+            image: "https://i.postimg.cc/nhDJZdk5/image.webp",
+        },
+        {
+            name: "clio2ph25",
+            description: "Full Blindado + 40 Tn de Peso",
+            image: "https://i.postimg.cc/zGgr5d71/image.png",
+        },
+        {
+            name: "paliop",
+            description: "Blindaje de Ruedas",
+            image: "https://i.postimg.cc/jS7bK8cY/image.png",
+        },
+        {
+            name: "captiva2010",
+            description: "Ruedas Blindadas",
+            image: "https://i.postimg.cc/FHL4mPpv/image.png",
+        },
+        {
+            name: "audia4vw",
+            description: "Full Blindado",
+            image: "https://i.postimg.cc/qv3T0mwT/image.png",
+        },
+        {
+            name: "204sR32Golf",
+            description: "Full Blindado",
+            image: "https://i.postimg.cc/tCbS7Bpv/image.png",
+        },
+        {
+            name: "evo9",
+            description: "Full Blindado",
+            image: "https://i.postimg.cc/02vWrVPF/image.png",
+        },
+        {
+            name: "venatusc",
+            description: "Ruedas Blindadas",
+            image: "https://i.postimg.cc/Vshs8bwf/image.png",
+        },
+        {
+            name: "shelby6x6cullinan",
+            description: "Full Blindado",
+            image: "https://i.postimg.cc/sD9hDcgc/image.png",
+        },
+        {
+            name: "m5e60",
+            description: "Addon Normal",
+            image: "https://i.postimg.cc/m2NH2wgm/image.png",
+        },
+        {
+            name: "cce60limit",
+            description: "Addon Normal",
+            image: "https://i.postimg.cc/4Nb9N5xP/image.png",
+        },
+        {
+            name: "gtrpit",
+            description: "Addon Normal",
+            image: "https://i.postimg.cc/ncYDckh6/image.png",
+        },
+        {
+            name: "Ikrownrcf",
+            description: "Ruedas Blindadas",
+            image: "https://i.postimg.cc/Hs4ys2LG/image.png",
+        },
+        {
+            name: "GODzEVOMR",
+            description: "Ruedas Blindadas",
+            image: "https://i.postimg.cc/KjjHvzn3/image.png",
+        },
+        {
+            name: "mansgtr50",
+            description: "Ruedas Blindadas",
+            image: "https://i.postimg.cc/2yyK864x/image.png",
+        },
+        {
+            name: "rs6abtkit",
+            description: "Full Blindado + Full Peso",
+            image: "https://i.postimg.cc/900nFMGY/image.png",
+        },
+        {
+            name: "ToraUSkyline ",
+            description: "Addon Normal",
+            image: "https://i.postimg.cc/3NNcJRg1/image.png",
+        },
+        {
+            name: "evo9",
+            description: "Ruedas Blindadas + 80 Tn de Peso",
+            image: "https://i.postimg.cc/VvvVL6jR/image.png",
+        }
+    ],
+
+    camionetas: [
+        {
+            name: "x6mhilux6x6",
+            description: "Full Blindado + 50 Tn de Peso",
+            image: "https://i.postimg.cc/TPvjpSJt/image.png",
+        },
+        {
+            name: "Fortuner",
+            description: "Full Blindado",
+            image: "https://i.postimg.cc/QdFsHYdC/image.png",
+        },
+        {
+            name: "overfinchsvr",
+            description: "Blindaje de ruedas y posee texturas bugueadas, que se arreglan en el mecanico",
+            image: "https://i.postimg.cc/rwxnn5h4/image.png",
+        },
+        {
+            name: "tonkat inferno",
+            description: "Full Blindado + full peso",
+            image: "https://i.postimg.cc/SQGzrR7R/image.png",
+        },
+        {
+            name: "DBmattrackh1",
+            description: "Blindaje de Ruedas + 60 Tn de Peso",
+            image: "https://i.postimg.cc/hGFjgRb0/image.png",
+        },
+        {
+            name: "titanhawk",
+            description: "Full Blindado + 50 Tn de Peso",
+            image: "https://i.postimg.cc/NjbcChC2/image.png",
+        },
+        {
+            name: "trhawk",
+            description: "Blindaje de ruedas + 30 Tn de Peso",
+            image: "https://i.postimg.cc/JhNM3DVn/image.png",
+        },
+        {
+            name: "x6m47",
+            description: "Blindaje de ruedas + Full Peso",
+            image: "https://i.postimg.cc/hGp3qcVb/image.png",
+        },
+        {
+            name: "bronco2021",
+            description: "Blindaje de ruedas + 80 Tn de Peso",
+            image: "https://i.postimg.cc/W1t7JgZ1/image.png",
+        },
+        {
+            name: "dbRedeyeTracky",
+            description: "Full Blindaje + 60 Tn de Peso",
+            image: "https://i.postimg.cc/KYMr0bJ1/image.png",
+        },
+        {
+            name: "captiva2010",
+            description: "Ruedas Blindadas",
+            image: "https://i.postimg.cc/FHL4mPpv/image.png",
+        },
+        {
+            name: "frontierbass",
+            description: "Addon Normal",
+            image: "https://i.postimg.cc/RFbsWmCb/image.png",
+        },
+        {
+            name: "addonmanu",
+            description: "Ruedas Blindadas",
+            image: "https://i.postimg.cc/yYKQWLs2/image.png",
+        },
+        {
+            name: "fbrrham",
+            description: "Ruedas Blindadas",
+            image: "https://i.postimg.cc/fy1Mj1CH/image.png",
+        },
+        {
+            name: "GODz22LIGHTNINGLFTD",
+            description: "Ruedas Blindadas",
+            image: "https://i.postimg.cc/DfNfTbhT/image.png",
+        },
+        {
+            name: "21WBTRXv2",
+            description: "Full Blindado + 80 Tn de Peso",
+            image: "https://i.postimg.cc/7PRPkT4y/image.png",
+        },
+        {
+            name: "4x4glc63",
+            description: "Full Blindado",
+            image: "https://i.postimg.cc/gj1Gq1s7/image.png",
+        },
+        {
+            name: "gcmf150svt2002",
+            description: "Ruedas Blindadas + 50 Tn de Peso",
+            image: "https://i.postimg.cc/WpypvJVd/image.png",
+        },
+        {
+            name: "21rav4",
+            description: "Full Blindado",
+            image: "https://i.postimg.cc/9XkXhq24/image.png",
+        },
+        {
+            name: "ikx3urus23",
+            description: "Ruedas Blindadas",
+            image: "https://i.postimg.cc/CM2Mgf05/image.png",
+        },
+        {
+            name: "sly4x4trackhawk",
+            description: "Ruedas Blindadas",
+            image: "https://i.postimg.cc/Pf7fj8Hw/image.png",
+        },
+        {
+            name: "21TRXSUV",
+            description: "Ruedas Blindadas + 70 Tn de Peso",
+            image: "https://i.postimg.cc/hPqmBwcs/image.png",
+        },
+        {
+            name: "DEFz_TRXEVO ",
+            description: "Full Blindado + Full Peso",
+            image: "https://i.postimg.cc/3JCvJBxZ/image.png",
+        }
+    ],
+
+    motos: [],
+
+    aereos: [],
+
+    otros: []
+};
+
+function showCategory(cat) {
+
+    // actualizar botones
+    document.querySelectorAll(".categories button").forEach(btn => {
+        btn.classList.remove("active");
+        if (btn.textContent.toLowerCase() === cat.toLowerCase()) {
+            btn.classList.add("active");
+        }
+    });
+
+    const container = document.getElementById("vehicle-container");
+    container.innerHTML = "";
+
+    vehicles[cat].forEach(v => {
+        const card = `
+        <div class="vehicle-card">
+            <img src="${v.image}" alt="${v.name}">
+            <h3>${v.name}</h3>
+            <p>${v.description}</p>
+        </div>`;
+        container.innerHTML += card;
+    });
+}
+
+showCategory("autos");
