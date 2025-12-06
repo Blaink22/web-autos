e8635b7a787fc484bce5a94fbd65e68cabc88d87
Blaink <v4lenj4u@gmail.com>
2025-12-06 19:28:40 -0300

    Update autos.js

--- js/autos.js
+++ js/autos.js
@@ -2,27 +2,27 @@ const vehicles = {
     autos: [
         {
             name: "bentayga4x4",
-            description: "Full Blindado",
+            description: "Adddon Normal",
             image: "https://i.postimg.cc/FH5NqgxL/bentayga4x4.png"
         },
         {
             name: "x6mf96lbwk",
-            description: "Blindaje de ruedas",
+            description: "Adddon Normal",
             image: "https://i.postimg.cc/2jQsyRdn/image.png"
         },
         {
             name: "surfpepsiv4",
-            description: "Blindaje de ruedas + suspension hidraulica manejable y posee texturas bugueadas, que se arreglan en el mecanico",
+            description: "Addon Normal + suspension hidraulica manejable y posee texturas bugueadas, que se arreglan en el mecanico",
             image: "https://i.postimg.cc/C1z64X98/image.png",
         },
         {
             name: "24mdxtech",
-            description: "Blindaje de ruedas",
+            description: "Blindaje a puertas cerradas",
             image: "https://i.postimg.cc/rFSZgSZR/image.png",
         },
         {
             name: "23x5",
-            description: "Full Blindaje + 50 Tn de Peso",
+            description: "Blindaje a puertas cerradas + 50 Tn de Peso",
             image: "https://i.postimg.cc/J4YNm0Fw/image.png",
         },
         {
@@ -31,68 +31,58 @@ const vehicles = {
             image: "https://i.postimg.cc/CxWbBtSF/image.png",
         },
         {
-            name: "enesperamocho",
-            description: "Full Blindaje +",
-            image: "https://i.postimg.cc/1X5F11cq/image.png",
-        },
-        {
             name: "nisgtir",
-            description: "Full Blindado",
+            description: "Blindaje a puertas cerradas",
             image: "https://i.postimg.cc/dtVf47dt/image.png",
         },
         {
             name: "ER34N",
-            description: "Blindaje de ruedas + full detalle con infinidad de calcomanias",
+            description: "Addon Normal + full detalle con infinidad de calcomanias",
             image: "https://i.postimg.cc/L51Jp4g9/image.png",
         },
         {
             name: "peugeot3008",
-            description: "Blindaje de ruedas",
+            description: "Blindaje a puertas cerradas",
             image: "https://i.postimg.cc/SjbJN0Wp/image.png",
         },
         {
             name: "isetta",
-            description: "Addon Normal",
+            description: "Blindaje a puertas cerradas",
             image: "https://i.postimg.cc/Cx1RNVwc/image.png",
         },
         {
             name: "yRenault5TB",
-            description: "Blindaje de ruedas",
+            description: "Addon Normal",
             image: "https://i.postimg.cc/g2TntB0K/image.png",
         },
         {
             name: "tbird",
-            description: "Blindaje de ruedas",
+            description: "Addon Normal",
             image: "https://i.postimg.cc/P5RPVFq4/image.png",
         },
         {
             name: "hycf40",
-            description: "Full Blindado",
+            description: "Addon Normal",
             image: "https://i.postimg.cc/L8bnQ0s3/image.png",
         },
         {
             name: "crossfoxjuani",
-            description: "Blindaje de ruedas",
+            description: "Addon Normal",
             image: "https://i.postimg.cc/nhDJZdk5/image.webp",
         },
         {
-            name: "clio2ph25",
-            description: "Full Blindado + 40 Tn de Peso",
-            image: "https://i.postimg.cc/zGgr5d71/image.png",
-        },
-        {
             name: "captiva2010",
-            description: "Ruedas Blindadas",
+            description: "Addon Normal",
             image: "https://i.postimg.cc/FHL4mPpv/image.png",
         },
         {
             name: "audia4vw",
-            description: "Full Blindado",
+            description: "Blindaje a puertas cerradas",
             image: "https://i.postimg.cc/qv3T0mwT/image.png",
         },
         {
             name: "evo9",
-            description: "Full Blindado",
+            description: "Addon Normal",
             image: "https://i.postimg.cc/02vWrVPF/image.png",
         },
         {
@@ -117,22 +107,22 @@ const vehicles = {
         },
         {
             name: "GODzEVOMR",
-            description: "Ruedas Blindadas",
+            description: "Addon Normal",
             image: "https://i.postimg.cc/KjjHvzn3/image.png",
         },
         {
             name: "mansgtr50",
-            description: "Ruedas Blindadas",
+            description: "Addon Normal",
             image: "https://i.postimg.cc/2yyK864x/image.png",
         },
         {
-            name: "ToraUSkyline ",
+            name: "ToraUSkyline",
             description: "Addon Normal",
-            image: "https://i.postimg.cc/3NNcJRg1/image.png",
+            image: "https://i.postimg.cc/DzYkKyyb/image.png",
         },
         {
             name: "992targa4s50",
-            description: "Ruedas Blindadas + 80 Tn de Peso",
+            description: "Addon Normal + 80 Tn de Peso",
             image: "https://i.postimg.cc/VvvVL6jR/image.png",
         }
     ],
@@ -140,77 +130,77 @@ const vehicles = {
     camionetas: [
         {
             name: "Fortuner",
-            description: "Full Blindado",
+            description: "Addon Normal",
             image: "https://i.postimg.cc/QdFsHYdC/image.png",
         },
         {
             name: "overfinchsvr",
-            description: "Blindaje de ruedas y posee texturas bugueadas, que se arreglan en el mecanico",
+            description: "Addon Normal que posee texturas bugueadas, que se arreglan en el mecanico",
             image: "https://i.postimg.cc/rwxnn5h4/image.png",
         },
         {
             name: "DBmattrackh1",
-            description: "Blindaje de Ruedas + 60 Tn de Peso",
+            description: "Addon Normal + 60 Tn de Peso",
             image: "https://i.postimg.cc/hGFjgRb0/image.png",
         },
         {
             name: "trhawk",
-            description: "Blindaje de ruedas + 30 Tn de Peso",
+            description: "Addon Normal + 30 Tn de Peso",
             image: "https://i.postimg.cc/JhNM3DVn/image.png",
         },
         {
             name: "bronco2021",
-            description: "Blindaje de ruedas + 80 Tn de Peso",
+            description: "Addon Normal + 80 Tn de Peso",
             image: "https://i.postimg.cc/W1t7JgZ1/image.png",
         },
         {
             name: "dbRedeyeTracky",
-            description: "Full Blindaje + 60 Tn de Peso",
+            description: "Blindaje a puertas cerradas + 60 Tn de Peso",
             image: "https://i.postimg.cc/KYMr0bJ1/image.png",
         },
         {
             name: "captiva2010",
-            description: "Ruedas Blindadas",
+            description: "Addon Normal",
             image: "https://i.postimg.cc/FHL4mPpv/image.png",
         },
         {
             name: "addonmanu",
-            description: "Ruedas Blindadas",
+            description: "Addon Normal",
             image: "https://i.postimg.cc/yYKQWLs2/image.png",
         },
         {
             name: "fbrrham",
-            description: "Ruedas Blindadas",
+            description: "Addon Normal",
             image: "https://i.postimg.cc/fy1Mj1CH/image.png",
         },
         {
             name: "GODz22LIGHTNINGLFTD",
-            description: "Ruedas Blindadas",
+            description: "Addon Normal",
             image: "https://i.postimg.cc/DfNfTbhT/image.png",
         },
         {
             name: "4x4glc63",
-            description: "Full Blindado",
+            description: "Blindaje a puertas cerradas + Infinidad de calcomanias",
             image: "https://i.postimg.cc/gj1Gq1s7/image.png",
         },
         {
             name: "gcmf150svt2002",
-            description: "Ruedas Blindadas + 50 Tn de Peso",
+            description: "Addon Normal + 50 Tn de Peso",
             image: "https://i.postimg.cc/WpypvJVd/image.png",
         },
         {
             name: "21rav4",
-            description: "Full Blindado",
+            description: "Blinaje a puerta cerrada",
             image: "https://i.postimg.cc/9XkXhq24/image.png",
         },
         {
             name: "ikx3urus23",
-            description: "Ruedas Blindadas",
+            description: "Addon Normal",
             image: "https://i.postimg.cc/CM2Mgf05/image.png",
         },
         {
             name: "sly4x4trackhawk",
-            description: "Ruedas Blindadas",
+            description: "Addon Normal",
             image: "https://i.postimg.cc/Pf7fj8Hw/image.png",
         }
     ],
