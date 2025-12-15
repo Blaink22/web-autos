ed8488a316befef533d4a6879650923dc6cb6079
Blaink <v4lenj4u@gmail.com>
2025-12-15 16:25:39 -0300

    Update casas.js

--- js/casas.js
+++ js/casas.js
@@ -63,25 +63,53 @@ const houses = [
     image: "https://i.postimg.cc/XN5DdTb1/screenshot-271.png"
   },
   {
+    name: "Casa Con Garage",
+    zone: "Sandy 3015",
+    type: "#888",
+    price: "$20.000",
+    image: "https://i.postimg.cc/SsLBk5DP/screenshot-274.png"
+  },
+  {
     name: "Casa Sin Garage",
-    zone: "Frente a GC",
-    type: "#854",
+    zone: "Paleto 1024",
+    type: "#896",
+    price: "$15.000",
+    image: "https://i.postimg.cc/yYNmHbPf/screenshot-275.png"
+  },
+  {
+    name: "Casa Sin Garage",
+    zone: "Paleto 1024 (puerta)",
+    type: "#900",
+    price: "$15.000",
+    image: "https://i.postimg.cc/QxsT8nJk/screenshot-276.png"
+  },
+  {
+    name: "Casa Sin Garage",
+    zone: "Paleto 1028",
+    type: "#925",
+    price: "$15.000",
+    image: "https://i.postimg.cc/Vv7mc6FH/screenshot-277.png"
+  },
+  {
+    name: "Casa Con Garage",
+    zone: "Cerca de Sulfu 8102",
+    type: "#969",
+    price: "$22.000",
+    image: "https://i.postimg.cc/hj52WRmg/screenshot-278.png"
+  },
+  {
+    name: "Casa Con Garage",
+    zone: "Cerca del Same 9336",
+    type: "#978",
+    price: "$21.000",
+    image: "https://i.postimg.cc/xCJ4ddYt/screenshot-279.png"
+  },
+  {
+    name: "Casa Sin Garage",
+    zone: "Cercano a Sulfu 8214",
+    type: "#999",
     price: "$18.000",
-    image: "https://i.postimg.cc/nhhy40Ty/screenshot-272.png"
-  },
-  {
-    name: "Casa Sin Garage",
-    zone: "Paleto, frente a polleria",
-    type: "#761",
-    price: "$15.000",
-    image: "https://i.postimg.cc/XN5DdTb1/screenshot-271.png"
-  },
-  {
-    name: "Casa Sin Garage",
-    zone: "Paleto, frente a polleria",
-    type: "#761",
-    price: "$15.000",
-    image: "https://i.postimg.cc/XN5DdTb1/screenshot-271.png"
+    image: "https://i.postimg.cc/TYCqZ6v9/screenshot-280.png"
   }
 ];
 
