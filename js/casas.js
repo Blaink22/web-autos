f59a3f05096ff50ec883157623b13b603c61ca24
Blaink <v4lenj4u@gmail.com>
2025-12-15 15:30:00 -0300

    Update casas.js

--- js/casas.js
+++ js/casas.js
@@ -36,31 +36,52 @@ const houses = [
   },
   {
     name: "Casa Sin Garage",
-    zone: "Paleto, frente a polleria",
-    type: "#504",
-    price: "$17.000",
-    image: "https://i.postimg.cc/W4w7WndT/screenshot-266.png"
+    zone: "Sulfu Zona Roja 8227",
+    type: "#672",
+    price: "$19.000",
+    image: "https://i.postimg.cc/X7Ddsv4r/screenshot-268.png"
+  },
+  {
+    name: "Casa Sin Garage",
+    zone: "Paleto frente a polleria 1025",
+    type: "#718",
+    price: "$15.000",
+    image: "https://i.postimg.cc/1R8vPbX7/screenshot-269.png"
+  },
+  {
+    name: "Casa Sin Garage",
+    zone: "A 2 cuadras de Flecca Ayunta 7117",
+    type: "#751",
+    price: "$15.000",
+    image: "https://i.postimg.cc/DzcMJs3S/screenshot-270.png"
   },
   {
     name: "Casa Sin Garage",
     zone: "Paleto, frente a polleria",
-    type: "#504",
-    price: "$17.000",
-    image: "https://i.postimg.cc/W4w7WndT/screenshot-266.png"
+    type: "#761",
+    price: "$15.000",
+    image: "https://i.postimg.cc/XN5DdTb1/screenshot-271.png"
+  },
+  {
+    name: "Casa Sin Garage",
+    zone: "Frente a GC",
+    type: "#854",
+    price: "$20.000",
+    image: "https://i.postimg.cc/nhhy40Ty/screenshot-272.png"
   },
   {
     name: "Casa Sin Garage",
     zone: "Paleto, frente a polleria",
-    type: "#504",
-    price: "$17.000",
-    image: "https://i.postimg.cc/W4w7WndT/screenshot-266.png"
+    type: "#761",
+    price: "$15.000",
+    image: "https://i.postimg.cc/XN5DdTb1/screenshot-271.png"
   },
   {
     name: "Casa Sin Garage",
     zone: "Paleto, frente a polleria",
-    type: "#504",
-    price: "$17.000",
-    image: "https://i.postimg.cc/W4w7WndT/screenshot-266.png"
+    type: "#761",
+    price: "$15.000",
+    image: "https://i.postimg.cc/XN5DdTb1/screenshot-271.png"
   }
 ];
 
